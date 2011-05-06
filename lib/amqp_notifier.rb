74f3827faf17bbf3729af861585935e64c92f553
Hugo Freire <hfreire@ntrglobal.com>
2011-05-06 11:36:46 +0200

    Change puts

--- lib/amqp_notifier.rb
+++ lib/amqp_notifier.rb
@@ -1,6 +1,6 @@
 class AmqpNotifier
   DEFAULT_EXCHANGE = 'labs_tests_1'
-  DEFAULT_OPTIONS = {:durable => true, :nowait => false, :type => :topic}
+  DEFAULT_OPTIONS = {:durable => true, :type => :topic}
   DEFAULT_PREFIX = ''
   DEFAULT_QUEUE = 'default'
   RETRIES = 10
@@ -13,12 +13,13 @@ class AmqpNotifier
   end
   
   def publish(key = '', message = '')
-    Rails.logger.error("#{key}:")
-    Rails.logger.error("#{message}")
+    $stderr.puts "#{key}:"
+    $stderr.puts "#{message}"
     Qusion.channel.topic(@exchange, @options).publish(message, :key => @key_prefix + key)
   end
   
   def subscribe(key = '', &block)
+    $stdout.puts "Subscriction to Exchange: #{@exchange} Queue: #{@queue} Key: #{key}"
     Qusion.channel.prefetch(1).queue(@queue, :durable => true).bind(Qusion.channel.topic(@exchange, @options), :key => key).subscribe(:ack => true) do |info, message|
       for i in (0..RETRIES)
         begin
@@ -26,8 +27,9 @@ class AmqpNotifier
           info.ack
           break
         rescue => e
-          Rails.logger.error("#{e.inspect}:")
-          Rails.logger.error("#{e.backtrace}")
+          $stderr.puts "Error executing block amqp"
+          $stderr.puts "#{e.inspect}:"
+          e.backtrace.each {|line| $stderr.puts line}
           if i == RETRIES
             AmqpNotifier.new("#{@exchange}_errors").publish(key, message)
             info.reject(:requeue => false)
